0603919473f51d0fb3db2c1a7ee937feb4151455
noteinabottle1 <liah.faile1@gmail.com>
2023-12-17 20:24:00 -0500

    fanfiction.net URLs may not have exactly 8 numbers

--- util/util.py
+++ util/util.py
@@ -8,11 +8,11 @@ def lprint(info, end="\n"):
     print(msg, end=end)
 
 def validate_url(url):
-    if bool(re.fullmatch("^https://www.fanfiction.net/s/[0-9]{8}(.*)$", url)):
+    if bool(re.fullmatch("^https://www.fanfiction.net/s/[0-9]*(.*)$", url)):
         return '/'.join(map(str, url.split("/")[:5]))
-    elif bool(re.fullmatch('^www.fanfiction.net/s/[0-9]{8}(.*)$', url)):
+    elif bool(re.fullmatch('^www.fanfiction.net/s/[0-9]*(.*)$', url)):
         return 'https://' + '/'.join(map(str, url.split("/")[:3]))
-    elif bool(re.fullmatch('^fanfiction.net/s/[0-9]{8}(.*)$', url)):
+    elif bool(re.fullmatch('^fanfiction.net/s/[0-9]*(.*)$', url)):
         return 'https://www.' + '/'.join(map(str, url.split("/")[:3]))
     elif bool(re.fullmatch("^[0-9]{8}$", url)):
         return f'https://www.fanfiction.net/s/{url}'
